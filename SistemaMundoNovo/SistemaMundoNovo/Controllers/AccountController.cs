79ccb97722f3081278e6651463cba276c46a8fd3
AmaliaCristina <arcoresdiva@hotmail.com>
2018-09-21 16:23:12 -0300

    alterando barra navegação

--- SistemaMundoNovo/SistemaMundoNovo/Controllers/AccountController.cs
+++ SistemaMundoNovo/SistemaMundoNovo/Controllers/AccountController.cs
@@ -176,7 +176,7 @@ namespace SistemaMundoNovo.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirmar sua conta", "Confirme sua conta clicando <a href=\"" + callbackUrl + "\">aqui</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Categorias");
                 }
                 AddErrors(result);
             }
